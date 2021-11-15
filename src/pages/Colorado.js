346b6a9e088bdbce0d5ef27233f5e37bb02aaef3
davidvader <davelvader@gmail.com>
2021-11-15 15:43:25 -0600

    carousel

--- src/pages/Colorado.js
+++ src/pages/Colorado.js
@@ -3,8 +3,7 @@
 // Import Swiper styles
 // Import Swiper React components
 import { Carousel } from 'react-responsive-carousel';
-import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
-import { Navigation, Pagination, Scrollbar } from 'swiper';
+import "react-responsive-carousel/lib/styles/carousel.min.css";
 import Clue from '../Clue.js';
 import City1 from '../img/colorado/city1.jpg';
 import City2 from '../img/colorado/city2.jpg';
@@ -17,6 +16,7 @@ import Food3 from '../img/colorado/food3.jpg';
 import Mtn1 from '../img/colorado/mtn1.jpg';
 import Mtn2 from '../img/colorado/mtn2.jpg';
 import Mtn3 from '../img/colorado/mtn3.jpg';
+import Mtn4 from '../img/colorado/mtn4.jpg';
 import Us1 from '../img/colorado/us1.jpg';
 import Us2 from '../img/colorado/us2.jpg';
 import './Pages.css';
@@ -34,69 +34,65 @@ function Colorado() {
             <img width="" src="https://www.planetware.com/photos-tiles/colorado-denver-city-view-and-mountains.jpg" alt="denver"/>
             <p>When can we move there?
             </p>
-            <Carousel
-                  modules={[Navigation, Pagination, Scrollbar ]}
-                  navigation
-                  pagination={{ clickable: true }}
-                  scrollbar={{ draggable: true }}
-                  loop
-      spaceBetween={150}
-      slidesPerView={1}
-    //   onSlideChange={() => console.log('slide change')}
-    //   onSwiper={(swiper) => console.log(swiper)}
-    >
+            <Carousel autoPlay infiniteLoop centerMode>
                     <div className="slide-content">
-                    <img src={Us1} alt="us1" height="360px"/>
-                    <p className="legend">Red Rocks</p>
+                    <div><img src={Us1} alt="us1" /></div>
+                    <p className="legend">Cuties in the Red Rocks</p>
                     </div>
                 <div className="slide-content">
-                    <img src={Us2} alt="us2" height="360px"/>
-                    <p className="legend">Nice and Quiet AirBnB</p>
+                    <div><img src={Us2} alt="us2" /></div>
+                    <p className="legend">What a wonderful place to stay</p>
                     </div>
                 <div className="slide-content">
-                    <img src={City1} alt="city1" height="360px"/>
-                    <p className="legend">Fun in the city</p>
+                    <div><img src={City1} alt="city1" /></div>
+                    <p className="legend">Cheesin' at the rocks</p>
                     </div>
                 <div className="slide-content">
-                    <img src={City2} alt="city2" height="360px"/>
-                    <p className="legend">More cutie in the Red Rocks</p>
-                    </div>
-                <div className="slide-content">
-                    <img src={City3} alt="city3" height="360px"/>
-                    <p className="legend">Fun in the city</p>
-                    </div>
-                <div className="slide-content">
-                    <img src={City4} alt="city4" height="360px"/>
-                    <p className="legend">Fun in the city</p>
-                    </div>
-                <div className="slide-content">
-                    <img src={City5} alt="city5" height="360px"/>
-                    <p className="legend">Fun in the city</p>
-                    </div>
-                <div className="slide-content">
-                    <img src={Food1} alt="Food1" height="360px"/>
-                    <p className="legend">Fun in the city</p>
-                    </div>
-                <div className="slide-content">
-                    <img src={Food2} alt="Food2" height="360px"/>
-                    <p className="legend">Fun in the city</p>
-                    </div>
-                <div className="slide-content">
-                    <img src={Food3} alt="Food3" height="360px"/>
-                    <p className="legend">Fun in the city</p>
-                    </div>
-                <div className="slide-content">
-                    <img src={Mtn1} alt="Mtn1" height="360px"/>
-                    <p className="legend">Fun in the city</p>
-                    </div>
-                <div className="slide-content">
-                    <img src={Mtn2} alt="Mtn2" height="360px"/>
-                    <p className="legend">Fun in the city</p>
+                    <div><img src={City2} alt="city2" /></div>
+                    <p className="legend">Gettin brews with my boos</p>
                     </div>
 
                 <div className="slide-content">
-                    <img src={Mtn3} alt="Mtn3" height="360px"/>
-                    <p className="legend">Fun in the city</p>
+                    <div><img src={City4} alt="city4" /></div>
+                    <p className="legend">I wouldn't mess with them</p>
+                    </div>
+                <div className="slide-content">
+                    <div><img src={City5} alt="city5" /></div>
+                    <p className="legend">Gay means cool</p>
+                    </div>
+                    <div className="slide-content">
+                    <div><img src={City3} alt="city3" /></div>
+                    <p className="legend">Solidarity!</p>
+                    </div>
+                <div className="slide-content">
+                    <div><img src={Food1} alt="Food1" /></div>
+                    <p className="legend">Best. Breakfast. Ever.</p>
+                    </div>
+                <div className="slide-content">
+                    <div><img src={Food2} alt="Food2" /></div>
+                    <p className="legend">My view, is the best view.</p>
+                    </div>
+                <div className="slide-content">
+                    <div><img src={Food3} alt="Food3" /></div>
+                    <p className="legend">Scrumptious sweets for the sweetie</p>
+                    </div>
+                <div className="slide-content">
+                    <div><img src={Mtn1} alt="Mtn1" /></div>
+                    <p className="legend">On top of the world</p>
+                    </div>
+                <div className="slide-content">
+                    <div><img src={Mtn2} alt="Mtn2" /></div>
+                    <p className="legend">Born to climb</p>
+                    </div>
+
+                    <div className="slide-content">
+                    <div><img src={Mtn3} alt="Mtn3" /></div>
+                    <p className="legend">My feet are doing <i>the thing</i></p>
+                    </div>
+
+                    <div className="slide-content">
+                    <div><img src={Mtn4} alt="Mtn4" /></div>
+                    <p className="legend">You were extremely good at that</p>
                     </div>
 
     </Carousel>
